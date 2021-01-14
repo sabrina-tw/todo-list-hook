f0d880e8def8f1423fe457e31b78dd99a2aaf097
Sabrina Sulong <sabrina.sulong@thoughtworks.com>
2021-01-14 11:15:56 +0800

    Display green tick image if todo item isDone

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,6 +3,15 @@ import React from "react";
 const Todo = ({ name, isDone }) => {
   return (
     <div>
+      <span>
+        {isDone && (
+          <img
+            src={`${process.env.PUBLIC_URL}/tick.png`}
+            alt="done"
+            height="20"
+          />
+        )}
+      </span>
       <span>{name}</span>
     </div>
   );
