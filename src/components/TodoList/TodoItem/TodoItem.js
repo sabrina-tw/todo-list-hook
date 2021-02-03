2c6268d64617ec1ad4a889bbf34b66619c593649
Sabrina Sulong <sabrina.sulong@thoughtworks.com>
2021-02-03 14:52:28 +0800

    Add tests for TodoItem

--- src/components/TodoList/TodoItem/TodoItem.js
+++ src/components/TodoList/TodoItem/TodoItem.js
@@ -6,13 +6,19 @@ const TodoItem = ({ name, isDone, id, toggleDone, deleteTodo }) => {
   return (
     <div className="todo-item">
       <span className="todo-item__completed" onClick={() => toggleDone(id)}>
-        {!isDone && <RadioButtonUnchecked />}
-        {isDone && <CheckCircle />}
+        {!isDone && (
+          <RadioButtonUnchecked data-testid="radio-button-unchecked-icon" />
+        )}
+        {isDone && <CheckCircle data-testid="check-circle-icon" />}
       </span>
       <span className={`todo-item__name ${isDone ? "completed" : ""}`}>
         {name}
       </span>
-      <Clear className="todo-item__delete" onClick={() => deleteTodo(id)} />
+      <Clear
+        data-testid="clear-icon"
+        className="todo-item__delete"
+        onClick={() => deleteTodo(id)}
+      />
     </div>
   );
 };
--- src/components/TodoList/TodoItem/TodoItem.test.js
+++ src/components/TodoList/TodoItem/TodoItem.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import TodoItem from "./TodoItem";
 
 describe("TodoItem", () => {
@@ -10,10 +10,56 @@ describe("TodoItem", () => {
     deleteTodo: jest.fn(),
   };
 
-  it("should render title of todo item", () => {
+  it("should render name of TodoItem", () => {
     render(<TodoItem {...todoItem} />);
+    const todoName = screen.getByText(todoItem.name);
 
-    const todoTitle = screen.getByText(todoItem.name);
-    expect(todoTitle).toBeInTheDocument();
+    expect(todoName).toBeInTheDocument();
+  });
+
+  it("should render CheckCircle icon if TodoItem is done", () => {
+    render(<TodoItem {...todoItem} />);
+    const checkedIcon = screen.getByTestId("check-circle-icon");
+
+    expect(checkedIcon).toBeInTheDocument();
+  });
+
+  it("should render RadioButtonUnchecked icon if TodoItem is not done", () => {
+    const undoneTodoItem = {
+      ...todoItem,
+      isDone: false,
+    };
+
+    render(<TodoItem {...undoneTodoItem} />);
+    const uncheckedIcon = screen.getByTestId("radio-button-unchecked-icon");
+
+    expect(uncheckedIcon).toBeInTheDocument();
+  });
+
+  describe("toggleDone", () => {
+    it("should call toggleDone when check icon is clicked", () => {
+      const toggleDone = jest.fn();
+      render(<TodoItem {...todoItem} toggleDone={toggleDone} />);
+
+      const checkedIcon = screen.getByTestId("check-circle-icon");
+      fireEvent.click(checkedIcon);
+
+      expect(toggleDone).toHaveBeenCalled();
+    });
+  });
+
+  describe("deleteTodo", () => {
+    it("should call deleteTodo when clear icon is clicked", async () => {
+      const deleteTodo = jest.fn();
+      render(<TodoItem {...todoItem} deleteTodo={deleteTodo} />);
+
+      const clearIcon = screen.getByTestId("clear-icon");
+      fireEvent.click(clearIcon);
+
+      expect(deleteTodo).toHaveBeenCalled();
+      // await waitFor(() => {
+      //   expect(screen.queryByText(/drink coffee/i)).not.toBeInTheDocument();
+      // });
+    });
   });
 });
